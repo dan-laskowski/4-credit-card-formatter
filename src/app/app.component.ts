50ad2a53c733833616b59d0a765aaf23e2b56ec3
dan-laskowski <daniel.laskowski94@gmail.com>
2023-11-14 09:59:28 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormatterComponent } from './components/formatter/formatter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormatterComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = '4-credit-card-formatter';
---
+++ src/app/components/formatter/formatter.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-formatter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './formatter.component.html',
+  styleUrl: './formatter.component.css'
+})
+export class FormatterComponent {
+
+}
